95cd712e567fd7d4562e31fc766426ae740f37ee
SanketBhole <90636671+SanketBhole@users.noreply.github.com>
2021-12-26 16:13:18 +0530

    Add files via upload

---
+++ Orders.php
@@ -0,0 +1,39 @@
+<?php
+if(isset($_POST['name'])){
+    // Set connection variables
+    $server = "sql210.unaux.com";
+    $username = "unaux_29969394";
+    $password = "1237898792951Sanket";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+
+    // Collect post variables
+    $mobile = $_POST['mobile'];
+    $name = $_POST['name'];
+	$email = $_POST['email'];
+	$orderdetails = $_POST['orderdetails'];
+	$sql = "INSERT INTO `unaux_29969394_sanket`.`Orders` (`Mobile`,`Name`, `Email`, `OrderDetails`) VALUES ('$mobile','$name', '$email', '$orderdetails');";
+    // echo $sql;
+
+    // Execute the query
+    if(mysqli_query($con, $sql)){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+    
+    // Close the database connection
+    $con->close();
+	}
+?>
